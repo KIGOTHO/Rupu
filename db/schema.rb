165fc8827ae1e7494f80c6d31f7aa47e10638018
KIGOTHO <mkigotho2@gmail.com>
2015-09-12 04:52:22 +0300

    crated the navigation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908154354) do
+ActiveRecord::Schema.define(version: 20150911234730) do
 
   create_table "deals", force: :cascade do |t|
     t.string   "description"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150908154354) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
