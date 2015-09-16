d5c16ab1ad4855085d3f47b25b8ec0268af21ce7
KIGOTHO <mkigotho2@gmail.com>
2015-09-16 18:43:54 +0300

    generated a model for the items in each category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911234730) do
+ActiveRecord::Schema.define(version: 20150916153808) do
 
   create_table "deals", force: :cascade do |t|
     t.string   "description"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 20150911234730) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.decimal  "price_from"
+    t.decimal  "price_to"
+    t.decimal  "price_save"
+    t.text     "description"
+    t.string   "image_url"
+    t.integer  "product_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "items", ["product_id"], name: "index_items_on_product_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
